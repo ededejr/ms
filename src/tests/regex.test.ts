0f79289e3f80dcd68c25c5dcf845838108e31c6a
Edede Oiwoh <eoiwoh@gmail.com>
2023-05-26 16:05:29 -0400

    test: add tests for am/pm

--- src/tests/regex.test.ts
+++ src/tests/regex.test.ts
@@ -1,12 +1,12 @@
-import { StandardRGX } from "../regexes"
-
-function makeTest(input: string, expected: boolean) {
-  test(`should ${!expected ? "not " : ''}match "${input}"`, () => {
-    expect(StandardRGX.matches(input)).toBe(expected);
-  });
-}
+import { AmPmRGX, StandardRGX } from "../regexes"
 
 describe('StandardRGX', () => {
+  function makeTest(input: string, expected: boolean) {
+    test(`should ${!expected ? "not " : ''}match "${input}"`, () => {
+      expect(StandardRGX.matches(input)).toBe(expected);
+    });
+  }
+
   describe('Valid time expressions', () => {
     makeTest("2 years 3 weeks 4 days 5 hours 6 minutes 7 seconds 8 milliseconds", true)
     makeTest("1 year 2 weeks 3 days", true)
@@ -58,3 +58,35 @@ describe('StandardRGX', () => {
     makeTest("0 years 0 weeks 0 days 0 hours 0 minutes 0 seconds 0 milliseconds", true)
   })
 })
+
+describe("AmPmRGX", () => {
+  function makeTest(input: string, expected: boolean) {
+    test(`should ${!expected ? "not " : ''}match "${input}"`, () => {
+      expect(AmPmRGX.matches(input)).toBe(expected);
+    });
+  }
+
+  makeTest("10AM", true)
+  makeTest("10:30AM", true)
+  makeTest("10:30 AM", true)
+  
+  makeTest("10:30PM", true)
+  makeTest("10:30 PM", true)
+  makeTest("10PM", true)
+
+  makeTest("10:30am", true)
+  makeTest("10:30 am", true)
+  makeTest("10am", true)
+
+  makeTest("10:30pm", true)
+  makeTest("10:30 pm", true)
+  makeTest("10pm", true)
+
+  makeTest("00:00pm", true);
+
+  makeTest("10:30", false)
+  makeTest("10", false)
+  makeTest("10:30:30", false)
+  makeTest("10:30:30AM", false)
+  makeTest("10:30:30PM", false)
+});
