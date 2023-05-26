3c40806a177ea6ffe0d179a9f568ca7511c8943b
Edede Oiwoh <eoiwoh@gmail.com>
2023-05-25 23:23:14 -0400

    fix: import issues after deletion

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 
 const ROOT_DIR = path.join(__dirname, "../");
+const SCRIPTS_DIR = path.join(ROOT_DIR, "scripts", "bin");
 const DIST_DIR = path.join(ROOT_DIR, "dist");
 
 export function getRootDir() {
@@ -10,6 +11,10 @@ export function getRootDir() {
 
 export function getDistDir() {
   return DIST_DIR;
+}
+
+export function getScriptsDir() {
+  return SCRIPTS_DIR;
 }
 
 export function ensureDistExists() {
